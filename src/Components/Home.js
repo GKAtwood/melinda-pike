84e540ab697f031c2ca5e8b7fd98e319b5b4029e
GKAtwood <gabrielle.k85@gmail.com>
2020-09-12 19:17:06 -0600

    styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -45,6 +45,20 @@ class Home extends Component {
             </div>
             </div>
           </section>
+          <section className ="about">
+          
+              <h1 className='abouttitle'>ABOUT MELINDA</h1>
+              <img src="https://images.unsplash.com/photo-1587754539999-b5f5492704c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="Melinda" className="melinda"   width="470"/>
+              <button className='aboutbutton'>READ MORE</button>
+              <div className='aboutcontent'>
+             
+            <p>My objective is to create a full funnel solution with expert Facebook & Instagram advertising services. We are a proficient Facebook Blueprint Certification partner offering top-notch services to small and big businesses alike with simple month-to-month contracts, à la carte service packages, and flexible budgets.
+        <br></br><br></br>
+“My job is to document memories of your most meaningful life events. My vision is to capture the little moments in between. Each image has the opportunity to tell a story and my mission is to present you with images that are timeless, which speak for themselves.” -Melinda
+</p>
+
+</div>
+          </section>
   
           <footer className='footer'>
               <h4>Copyright © 2020 Melinda Pike</h4>
